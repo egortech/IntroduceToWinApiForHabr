0ef12f4b1aca3b675dc83fb0d01d076dff5ac7eb
egortech <egortech@live.ru>
2016-09-11 23:48:26 +0300

    Create resource.h

---
+++ IntroduceToWinApiForHabr/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Resource.rc
+//
+#define IDMAINDIALOG                    9
+#define IDD_MAINDIALOG                  9
+#define IDC_XEDIT                       1001
+#define IDC_SECONDEDIT                  1003
+#define IDC_YEDIT                       1003
+#define IDC_CALCBUTTON                  1004
+#define IDC_CLEARBUTTON                 1005
+#define IDC_HEADERSTATIC                1006
+#define IDC_XSTATIC                     1007
+#define IDC_YSTATIC                     1008
+#define IDC_RESULTSTATIC                1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
